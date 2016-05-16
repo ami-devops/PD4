307bc8a1c2375f14af0c601d4da5c93bcfa3e7ea
ami-devops <amisha.patel@mindtree.com>
2016-05-16 17:03:31 -0400

    Update MeetingDao.java

--- src/com/mindtree/dao/MeetingDao.java
+++ src/com/mindtree/dao/MeetingDao.java
@@ -8,7 +8,7 @@ import com.mindtree.model.MeetingRoom;
 
 public interface MeetingDao 
 {
-	public abstract void addEmployee(Employee emp);
+//	public abstract void addEmployee(Employee emp);
 	public abstract List<Employee> getAllEmployees();
 	public abstract List<MeetingRoom> getAllMeetingRooms();
 	public abstract void addMeeting(Meeting meeting);
